6d4b0ed8848e720bbdb1697161eef34846bce13e
Fabian Furger <furgerfabian@hotmail.com>
2016-11-19 10:38:57 +0100

    Replace original script with generalized scripts

---
+++ kaggle.py
@@ -0,0 +1,170 @@
+import csv as csv
+import numpy as np
+import pandas as pd
+
+class Kaggle():
+  SEPARATOR = '-' * 80
+
+  def __init__(self, train_file, test_file, prediction_file, classifier):
+    self.train_file = train_file
+    self.test_file = test_file
+    self.prediction_file = prediction_file
+    self.unfitted_classifier = classifier
+
+    print('')
+    print('')
+    print('')
+    print(self.SEPARATOR)
+    print('Created new Kaggle instance')
+    print(self.SEPARATOR)
+
+  def _load_data(self):
+    train_df = pd.read_csv(self.train_file, header=0)
+    test_df = pd.read_csv(self.test_file, header=0)
+    print('Loaded training and test data')
+    return train_df, test_df
+
+  def _merge_data(self, data):
+    train_df, test_df = data
+    train_df['Train'] = True
+    test_df['Train'] = False
+    return pd.concat([train_df, test_df])
+
+  def split_data(self):
+    train = self.df.loc[(self.df.Train == True)].drop('Train', axis=1)
+    test = self.df.loc[(self.df.Train == False)].drop('Train', axis=1)
+    return train, test
+
+  def _engineer_features(self):
+    raise NotImplementedError('Feature engineering must be implemented in the derived class')
+
+  def remove_columns(self, columns):
+    """
+    Removes the specified additional columns from `self.df`.
+    """
+    print('Dropping columns %s' % columns)
+    self.df = self.df.drop(columns, axis=1)
+
+  def initialize(self):
+    """
+    Initializes the Kaggle by loading the data from the training and test CSV files and storing them
+    in a combined DataFrame. Then, the feature engineering steps are carried out, which depend on
+    the Kaggle implementation.
+    """
+    self.df = self._merge_data(self._load_data())
+    self._engineer_features()
+    print('Initialized Kaggle instance')
+    print(self.SEPARATOR)
+
+  def integerize_data(self, data):
+    for header in data:
+      type = data[header].dtype
+      if type != 'int64' and type != 'float64':
+        print('Integerizing row %s (%s)' % (header, type))
+        raise NotImplementedError()
+    return data
+
+  def _predict(self, train, predictor, test):
+    fitted_classifier = self.unfitted_classifier.fit(train, predictor)
+    return fitted_classifier.predict(test).astype(int), fitted_classifier
+
+  def predict_test_data(self, train, predictor, test, ids, header):
+    predictions, fitted_classifier = self._predict(train, predictor, test)
+
+    with open(self.prediction_file, 'wt') as predictions_file:
+      open_file_object = csv.writer(predictions_file)
+      open_file_object.writerow(header)
+      open_file_object.writerows(zip(ids, predictions))
+      predictions_file.close()
+
+    print('Predicted test data and written results to %s' % self.prediction_file)
+
+  def print_head(self):
+    print('Sample rows:')
+    print(self.SEPARATOR)
+    print(self.df.head(5))
+    print(self.SEPARATOR)
+
+  def cross_validate(self, data, predictor, folds=10):
+    fold_size = int(len(data)/folds)
+    features = data.columns
+
+    tps = []
+    fps = []
+    fns = []
+    tns = []
+    accs = []
+    precs = []
+    recs = []
+    f1s = []
+    importances = []
+
+    print('Starting %d-fold cross-validation with fold size %d based on features:\n%s' % (folds, fold_size, ', '.join(features)))
+
+    print('Fold', end=' ', flush=True)
+    for i in range(folds):
+      print(str(i), end='... ', flush=True)
+
+      # prepare training set
+      train = pd.concat([data[:i * fold_size:], data[(i+1) * fold_size:]])
+      labels = pd.concat([predictor[:i * fold_size:], predictor[(i+1) * fold_size:]])
+
+      # prepare test set
+      test = data[i*fold_size:(i+1)*fold_size]
+      answers = predictor[i*fold_size:(i+1)*fold_size]
+
+      predictions, forest = self._predict(train.values, labels.values, test.values)
+      tp = np.sum([(x == y == 1) for x, y in zip(predictions, answers)])
+      fp = np.sum([(x == 1 and y == 0) for x, y in zip(predictions, answers)])
+      fn = np.sum([(x == 0 and y == 1) for x, y in zip(predictions, answers)])
+      tn = np.sum([(x == y == 0) for x, y in zip(predictions, answers)])
+      if len(predictions) != fold_size or tp + fp + fn + tn != fold_size:
+        raise Error('Unexpected number of prediction results!!')
+
+      # print('    TP:  %f,\tFP:   %f,\tFN:  %f,\tTN: %f' % (tp/fold_size, fp/fold_size, fn/fold_size, tn/fold_size))
+      acc = (tp + tn) / fold_size
+      prec = tp / (tp + fp)
+      rec = tp / (tp + fn)
+      f1 = 2 * tp / (2 * tp + fp + fn)
+      # print('    acc: %f,\tprec: %f,\trec: %f,\tf1: %f' % (acc, prec, rec, f1))
+
+      tps.append(tp/fold_size)
+      fps.append(fp/fold_size)
+      fns.append(fn/fold_size)
+      tns.append(tn/fold_size)
+      accs.append(acc)
+      precs.append(prec)
+      recs.append(rec)
+      f1s.append(f1)
+      importances.append(forest.feature_importances_)
+
+    print('\n... finished running cros-validation!')
+    print(self.SEPARATOR)
+
+    return (tps, fps, fns, tns, accs, precs, recs, f1s, importances, features)
+
+  def evaluate_cross_validation_results(self, results):
+    tps, fps, fns, tns, accs, precs, recs, f1s, importances, features = results
+    print('Cross-validation results') # TODO: Add stdev
+    print(self.SEPARATOR)
+    print('TP:\tmin=%f\tmean=%f\tmax=%f' % (min(tps), sum(tps)/len(tps), max(tps)))
+    print('FP:\tmin=%f\tmean=%f\tmax=%f' % (min(fps), sum(fps)/len(fps), max(fps)))
+    print('FN:\tmin=%f\tmean=%f\tmax=%f' % (min(fns), sum(fns)/len(fns), max(fns)))
+    print('TN:\tmin=%f\tmean=%f\tmax=%f' % (min(tns), sum(tns)/len(tns), max(tns)))
+    print('ACC:\tmin=%f\tmean=%f\tmax=%f' % (min(accs), sum(accs)/len(accs), max(accs)))
+    print('PREC:\tmin=%f\tmean=%f\tmax=%f' % (min(precs), sum(precs)/len(precs), max(precs)))
+    print('REC:\tmin=%f\tmean=%f\tmax=%f' % (min(recs), sum(recs)/len(recs), max(recs)))
+    print('F1:\tmin=%f\tmean=%f\tmax=%f' % (min(f1s), sum(f1s)/len(f1s), max(f1s)))
+    print(self.SEPARATOR)
+    mean_importance_ranking = []
+    for i in range(len(importances[0])):
+      imp = []
+      for j in importances:
+        imp.append(j[i])
+      mean = sum(imp)/len(imp)
+      print('IMP(%d):\tmin=%f\tmean=%f\tmax=%f' % (i, min(imp), mean, max(imp)))
+      mean_importance_ranking.append((mean, i))
+    mean_importance_ranking.sort()
+    print('Mean importance ranking: \n%s' % '\n'.join(list(map(lambda x: '%s: %f' % (features[x[1]].ljust(16), x[0]), mean_importance_ranking))))
+    print(self.SEPARATOR)
+
---
+++ titanic.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+from kaggle import Kaggle
+
+class TitanicKaggle(Kaggle):
+  def __init__(self, classifier):
+    Kaggle.__init__(self, 'data/train.csv', 'data/test.csv', 'prediction.csv', classifier) # TODO: determine base class dynamically
+
+  def _engineer_features(self):
+    print('Preparing data...')
+
+    print('- Converting gender...')
+    genders = {'female': 0, 'male': 1}
+    self.df['Gender'] = self.df['Sex'].map(genders)
+
+    print('- Converting ports...')
+    most_common_port = self.df.Embarked.dropna().mode().values[0]
+    print('  using most common port "%s" for null-entries' % most_common_port)
+    self.df.loc[self.df.Embarked.isnull(), 'Embarked'] = most_common_port
+    ports = list(enumerate(np.unique(self.df['Embarked'])))
+    ports_dict = {name: i for i, name in ports}
+    self.df.Embarked = self.df.Embarked.map(ports_dict)
+
+    print('- Preparing fares...')
+    mean_fares = np.zeros((3))
+    print('  calculating class-based mean fares')
+    for i in range(0, 3):
+      mean_fares[i] = self.df[self.df.Pclass == i+1].Fare.mean()
+    print('  filling fares with means')
+    for i in range(0, 3):
+      self.df.loc[(self.df.Fare.isnull()) & (self.df.Pclass == i+1), 'Fare'] = mean_fares[i]
+
+    print('- Preparing ages...')
+    median_ages = np.zeros((2,3))
+    print('  calculating gender- and class-based median ages')
+    for i in range(0, 2):
+      for j in range(0, 3):
+        median_ages[i,j] = self.df[(self.df.Gender == i) & (self.df.Pclass == j+1)].Age.dropna().median()
+    print('  filling missing ages with medians')
+    for i in range(0, 2):
+      for j in range(0, 3):
+        self.df.loc[(self.df.Age.isnull()) & (self.df.Gender == i) & (self.df.Pclass == j+1), 'Age'] = median_ages[i,j]
+    age_bins = np.linspace(self.df.Age.min(),self.df.Age.max(), 20)
+    # print('  grouping ages with bins: %s' % age_bins)
+    self.df['AgeGroup'] = np.digitize(self.df.Age, age_bins)
+
+    print('- Preparing families...')
+    self.df['FamilySize'] = self.df.SibSp + self.df.Parch
+
+    print('- Preparing family groups...')
+    self.df['FamilyGroup'] = self.df.FamilySize.map(lambda s: 0 if s == 0 else 2 if s > 3 else 1)
+
+    print('- Preparing titles...')
+    self.df['Title'] = self.df.Name.map(lambda n: re.sub('(.*, )|(\\..*)', '', n))
+    self.df.loc[(self.df.Title == 'Mlle'), 'Title'] = 'Miss'
+    self.df.loc[(self.df.Title == 'Ms'), 'Title'] = 'Miss'
+    self.df.loc[(self.df.Title == 'Mme'), 'Title'] = 'Miss'
+    rare_titles = ['Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer']
+    self.df.loc[(self.df.Title.isin(rare_titles)), 'Title'] = 'Rare title'
+    titles = {'Rare title': 0, 'Master': 1, 'Mr': 2, 'Miss': 3, 'Mrs': 4}
+    self.df.Title = self.df.Title.map(titles).astype(int)
+    # group = self.df.Title.groupby(self.df.Title).agg([np.count_nonzero])
+    # print(group)
+    print('... done preparing data!')
+    print(self.SEPARATOR)
+
+  def print_stats(self):
+    print('Calculating some statistics...')
+    total_passengers = len(self.df.index)
+    survivor_count = len (self.df[self.df.Survived == 1])
+    print('- Survivors/passengers (ratio): %d/%d (%f)' % (survivor_count, total_passengers, survivor_count/total_passengers))
+
+    women_on_board =  len(self.df[self.df.Gender == 0])
+    female_survivors = len(self.df[(self.df.Survived == 1) & (self.df.Gender == 0)])
+    men_on_board =  len(self.df[self.df.Gender == 1])
+    male_survivors = len(self.df[(self.df.Survived == 1) & (self.df.Gender == 1)])
+    print('- Women/men on board (survival rate): %d/%d (%f/%f)' % (women_on_board, men_on_board, female_survivors/women_on_board, male_survivors/men_on_board))
+
+    class_counts = np.zeros(3)
+    for i in range(0, 3):
+      class_counts[i] = len(self.df[self.df.Pclass == i+1])
+    class_survival = np.zeros((2,3))
+    for i in range(0, 2):
+      for j in range(0, 3):
+        class_survival[i,j] = len(self.df[(self.df.Survived == i) & (self.df.Pclass == j+1)]) / class_counts[j]
+    print('- Chance of survival by class:')
+    print(class_survival)
+
+    embark_counts = np.zeros(3)
+    for i in range(0, 3):
+      embark_counts[i] = len(self.df[self.df.Embarked == i])
+    #print('- Number of embarkers per port: %s' % embark_counts)
+
+    embark_survival = np.zeros((2,3))
+    for i in range(0, 2):
+      for j in range(0, 3):
+        embark_survival[i,j] = len(self.df[(self.df.Survived == i) & (self.df.Embarked == j)]) / embark_counts[j]
+    print('- Chance of survival by port:')
+    print(embark_survival)
+
+    print('- Unique siblings: %s' % self.df.SibSp.unique())
+    for sib in self.df.SibSp.unique():
+      counts = len(self.df[self.df.SibSp == sib])
+      print('%d siblings: %d times, rate: %f' % (sib, counts, len(self.df[(self.df.SibSp == sib) & (self.df.Survived == 1)]) / counts))
+
+    print('- Unique parch: %s' % self.df.Parch.unique())
+    for parch in self.df.Parch.unique():
+      counts = len(self.df[self.df.Parch == parch])
+      print('%d parchlings: %d times, rate: %f' % (parch, counts, len(self.df[(self.df.Parch == parch) & (self.df.Survived == 1)]) / counts))
+
+    print('- Unique family size: %s' % self.df.FamilySize.unique())
+    for family in self.df.FamilySize.unique():
+      counts = len(self.df[self.df.FamilySize == family])
+      print('%d familylings: %d times, rate: %f' % (family, counts, len(self.df[(self.df.FamilySize == family) & (self.df.Survived == 1)]) / counts))
+
+    print('- Unique groups: %s' % self.df.FamilyGroup.unique())
+    for family in self.df.FamilyGroup.unique():
+      counts = len(self.df[self.df.FamilyGroup == family])
+      print('family group %d: %d times, rate: %f' % (family, counts, len(self.df[(self.df.FamilyGroup == family) & (self.df.Survived == 1)]) / counts))
+
+    print('- Unique titles: %s' % self.df.Title.unique())
+    for title in self.df.Title.unique():
+      counts = len(self.df[self.df.Title == title])
+      print('title %d: %d times, rate: %f' % (title, counts, len(self.df[(self.df.Title == title) & (self.df.Survived == 1)]) / counts))
+
+    print('... done printing statistics!')
+    print(self.SEPARATOR)
+
+
+# set up
+titanic = TitanicKaggle(RandomForestClassifier(n_estimators=100))
+titanic.initialize()
+
+# analyze
+titanic.print_head()
+titanic.print_stats()
+
+# prepare for prediction
+train, test = titanic.split_data()
+
+passenger_id = 'PassengerId'
+survived = 'Survived'
+header = [passenger_id, survived]
+predictor = train[survived]
+ids = test[passenger_id]
+
+features = ['Embarked', 'Parch', 'SibSp', 'Pclass', 'Gender', 'AgeGroup', 'FamilySize', 'FamilyGroup', 'Title']
+train = titanic.integerize_data(train[features])
+test = titanic.integerize_data(test[features])
+
+# predict
+results = titanic.cross_validate(train, predictor)
+titanic.evaluate_cross_validation_results(results)
+titanic.predict_test_data(train, predictor, test, ids, header)
+
