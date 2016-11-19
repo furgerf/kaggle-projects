ccfd1003d71cb330d993ac84f8f2e817785aa50d
Fabian Furger <furgerfabian@hotmail.com>
2016-11-19 20:43:44 +0100

    Added another level of brute-force feature set generation and filtering out duplicates

--- feature_finder.py
+++ feature_finder.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from kaggle import Kaggle
 from cross_validation_result import CrossValidationResult
 
@@ -9,26 +11,30 @@ class FeatureFinder():
 
   def _generate_feature_sets(self, all_features):
     # TODO: Improve...
-    self.features_sets = []
-    self.features_sets.append(all_features)
+    feature_sets = []
+    feature_sets.append(all_features)
     for i in range(len(all_features)):
       new_feature_set = all_features.copy()
       new_feature_set.remove(all_features[i])
-      self.features_sets.append(new_feature_set)
+      feature_sets.append(new_feature_set)
 
-      """
       for j in range(len(all_features) - 1):
         very_new_feature_set = new_feature_set.copy()
         very_new_feature_set.remove(new_feature_set[j])
-        self.features_sets.append(very_new_feature_set)
-      """
+        feature_sets.append(very_new_feature_set)
 
-    print('Generated %d different feature sets' % len(self.features_sets))
+        for k in range(len(all_features) - 2):
+          very_very_new_feature_set = very_new_feature_set.copy()
+          very_very_new_feature_set.remove(very_new_feature_set[k])
+          feature_sets.append(very_very_new_feature_set)
+
+    self.feature_sets = np.unique(feature_sets)
+    print('Generated %d different feature sets' % len(self.feature_sets))
 
   def run_predictions(self):
     results = {}
-    for i, features in enumerate(self.features_sets):
-      print('Processing feature set %d/%d' % (i+1, len(self.features_sets)))
+    for i, features in enumerate(self.feature_sets):
+      print('Processing feature set %d/%d' % (i+1, len(self.feature_sets)))
 
       accuracies = []
       f1_scores = []
@@ -38,7 +44,7 @@ class FeatureFinder():
         train_predictors = train_data[kaggle.PREDICTOR_COLUMN_NAME]
         train_data = Kaggle.numericalize_data(train_data[features])
 
-        cv_result = kaggle.cross_validate(train_data, train_predictors, silent=True, folds=2)
+        cv_result = kaggle.cross_validate(train_data, train_predictors, silent=True, folds=10)
 
         accuracies.extend(cv_result.accuracies)
         f1_scores.extend(cv_result.f1_scores)
