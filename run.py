05dc80c83b9563bfab6be4f2e63246db58968a04
Fabian Furger <furgerfabian@hotmail.com>
2016-11-19 18:58:20 +0100

    Structuring run.py

--- run.py
+++ run.py
@@ -6,70 +6,65 @@ from sklearn.ensemble import RandomForestClassifier
 from cross_validation_result import CrossValidationResult
 from titanic import TitanicKaggle
 
+def experiment():
+  # set up experiment titanic instance
+  titanic = TitanicKaggle(lambda: RandomForestClassifier(n_estimators=100, random_state=123))
+  titanic.initialize()
+
+  # analyze
+  titanic.print_sample_data()
+  titanic.analyze_data()
+
+  # prepare data for prediction
+  train, test = titanic.split_data()
+  survived = 'Survived'
+  predictor = train[survived]
+
+  features = ['Embarked', 'Parch', 'SibSp', 'Pclass', 'Gender', 'AgeGroup', 'FamilySize', 'FamilyGroup', 'Title']
+  prepared_train_data = TitanicKaggle.numericalize_data(train[features])
+  prepared_test_data = TitanicKaggle.numericalize_data(test[features])
+
+  titanic.cross_validate(prepared_train_data, predictor).print_results()
+
+def predict_test_data():
+  # set up experiment titanic instance
+  titanic = TitanicKaggle(lambda: RandomForestClassifier(n_estimators=100, random_state=123))
+  titanic.initialize()
+
+  # prepare data for prediction
+  train, test = titanic.split_data()
+  survived = 'Survived'
+  passenger_id = 'PassengerId'
+  predictor = train[survived]
+  ids = test[passenger_id]
+
+  survived = 'Survived'
+  header = [passenger_id, survived]
+
+  features = ['Embarked', 'Parch', 'SibSp', 'Pclass', 'Gender', 'AgeGroup', 'FamilySize', 'FamilyGroup', 'Title']
+  prepared_train_data = TitanicKaggle.numericalize_data(train[features])
+  prepared_test_data = TitanicKaggle.numericalize_data(test[features])
+
+  titanic.predict_test_data(prepared_train_data, predictor, prepared_test_data, ids, header)
+
+
 # set up
 start_time = datetime.utcnow()
 print('')
 print('')
 print('')
+print(TitanicKaggle.SEPARATOR)
 print('Evaluation start time: %s' % start_time)
-titanic = TitanicKaggle(lambda: RandomForestClassifier(n_estimators=100, random_state=123))
-titanic.initialize()
-
-# analyze
-# titanic.print_sample_data()
-# titanic.analyze_data()
-
-# prepare for prediction
-train, test = titanic.split_data()
-
-passenger_id = 'PassengerId'
-survived = 'Survived'
-header = [passenger_id, survived]
-predictor = train[survived]
-ids = test[passenger_id]
-
-all_features = ['Embarked', 'Parch', 'SibSp', 'Pclass', 'Gender', 'AgeGroup', 'FamilySize', 'FamilyGroup', 'Title']
-features_sets = []
-
-features_sets.append(all_features)
-for i in range(len(all_features)):
-  new_feature_set = all_features.copy()
-  new_feature_set.remove(all_features[i])
-  features_sets.append(new_feature_set)
-
-  for j in range(len(all_features) - 1):
-    very_new_feature_set = new_feature_set.copy()
-    very_new_feature_set.remove(new_feature_set[j])
-    features_sets.append(very_new_feature_set)
-
-five_fold_results = []
-ten_fold_results = []
-
-for features in features_sets:
-  current_train = TitanicKaggle.numericalize_data(train[features])
-  current_test = TitanicKaggle.numericalize_data(test[features])
-
-  # predict
-  five_fold_results.append(titanic.cross_validate(current_train, predictor, folds=5))
-  ten_fold_results.append(titanic.cross_validate(current_train, predictor, folds=10))
-
-print(TitanicKaggle.SEPARATOR)
-for i, features in enumerate(features_sets):
-  print('Features: %s' % features)
-  # five_fold_results[i].print_short_results()
-  # ten_fold_results[i].print_short_results()
-
-  accuracies = [CrossValidationResult.mean(five_fold_results[i].accuracies), CrossValidationResult.mean(ten_fold_results[i].accuracies)]
-  f1_scores = [CrossValidationResult.mean(five_fold_results[i].f1_scores), CrossValidationResult.mean(ten_fold_results[i].f1_scores)]
-
-  print('AVG:\t\t\tACC=%f\tF1=%f' % (CrossValidationResult.mean(accuracies), CrossValidationResult.mean(f1_scores)))
-  print(TitanicKaggle.SEPARATOR)
 print(TitanicKaggle.SEPARATOR)
 
-# titanic.predict_test_data(train, predictor, test, ids, header)
+
+experiment()
+predict_test_data()
+
 
 end_time = datetime.utcnow()
 duration = end_time - start_time
 print('Evaluation end time: %s' % end_time)
 print('Total evaluation duration: %s' % duration)
+print(TitanicKaggle.SEPARATOR)
 
