e80a10fe01df2687adc7632bb2afaa0dcec05cbe
Fabian Furger <fabian.furger@hexagonmining.com>
2017-03-28 06:03:14 +0000

    Various changes

--- cancer/scan.py
+++ cancer/scan.py
@@ -1,8 +1,15 @@
 import pickle
+import numpy as np
 
 class Scan(object):
 
   def __init__(self, scan_id, label=None, data_index=None):
+    """
+    scan_id:    (string) ID of the scan, this is expected to be the file name
+    label:      (any)    Label of the scan, defaults to None (label unknown)
+    data_index: (int)    Index to use when retrieving data from pickle (for tuple/array),
+                         defaults to None (apply no index)
+    """
     self.scan_id = scan_id
     self.data_index = data_index
     self.label = label
@@ -15,6 +22,7 @@ class Scan(object):
       with open('./preprocessed/{}.pickle'.format(self.scan_id), "rb") as f:
         tmp = pickle.load(f)
         self._data = tmp[self.data_index] if self.data_index is not None else tmp
+        self._data = np.array(self._data) / 1000
 
     return self._data
 
--- cancer/tf_network.py
+++ cancer/tf_network.py
@@ -1,13 +1,23 @@
 import tensorflow as tf
 
 class TfNetwork(object):
+
   def __init__(self, dimensionality):
+    """
+    Creates a new TfNetwork. Note that it is apparently required to define
+    variables W and b even if they'll be later restored from a previous session.
+
+    dimensionality: (int) Number of dimensions (features) for the network
+    """
     self.x = tf.placeholder(tf.float32, [None, dimensionality])
-    self.W = tf.Variable(tf.zeros([dimensionality, 1]), name='W')
-    self.b = tf.Variable(tf.zeros([1]), name='b')
+    self.W = tf.Variable(tf.random_normal([dimensionality, 1]), name='W')
+    self.b = tf.Variable(tf.random_normal([1]), name='b')
     self.y = tf.matmul(self.x, self.W) + self.b
 
   def load_variables(self, session):
+    """
+    Loads variables W and b from the provided session.
+    """
     self.b = None
     self.W = None
     variables = tf.trainable_variables()
@@ -17,7 +27,9 @@ class TfNetwork(object):
         print('Found b', self.b, self.b.eval(session))
       if variable.name == 'W:0':
         self.W = variable
-        print('Found W', self.W, self.W.eval(session))
+        w = self.W.eval(session)
+        print('Found W', self.W, w.min(), '-', w.max(), '~', w.mean())
     if self.b == None or self.W == None:
-      raise ValueError('No tensors b or W found')
+      raise ValueError('Variable b or W was not found')
+    self.y = tf.matmul(self.x, self.W) + self.b
 
